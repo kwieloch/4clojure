30ab23f0855dc04f8d8d2bf02b44942ba2fca117
Karol Wieloch <kwieloch@gmail.com>
2018-10-02 11:11:11 +0200

    customize descriptions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject four-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Solution to some of the 4clojure.com problems"
+  :url "https://github.com/kwieloch/four-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
